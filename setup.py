301c92706216b009f65698b3f17ab1e8463cbf0f
Jago Strong-Wright <jagoosw@protonmail.com>
2021-02-12 01:29:40 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="narpy - NASA AMES Reader for Python",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="Jago Strong-Wright",
+    author_email="jagoosw@protonmail.com",
+
+    # Packages
+    packages=["narpy"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="na",
+
+    #
+    license="LICENSE.txt",
+    description="A simple NASA AMES file format reader for python",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "numpy",
+    ],
+)
